5f59f5eeacaa93f533c739c3dbbcfdb759aad754
Dylan Slogrove <dslogrove@gmail.com>
2019-05-31 15:15:42 +0200

    Finally fixed desync issues

--- BetterCat.py
+++ BetterCat.py
@@ -31,11 +31,15 @@ def handle_args():
 
 	return parser.parse_args()
 
-def remote_command(command, server, timeout=0.2):
+def remote_command(command, server, timeout=2, prompt=None):
 	_ = server.send_line(str.encode(command))
-	output = server.recv(timeout=timeout)
-	result = bytes.decode(output, 'ascii')
-	return result
+	if (prompt):
+		output = server.recv_until(str.encode(prompt))
+	else:
+		output = server.recv(timeout=timeout)
+		if (str(output, 'ascii').strip().endswith(command.strip())):
+			output = server.recv(timeout=timeout)
+	return bytes.decode(output, 'ascii')
 
 def spawn_tty(server, shell):
 	#TODO: other methods of tty spawning
@@ -67,42 +71,31 @@ def check_term():
 	color = "color" in term
 	return term, color
 	
-def set_term(term, server):
+def set_term(term, server, prompt):
 	fallback = ['dumb','xterm', term]
-	_ = remote_command('reset', server, 5)
-	#recv_until will ignore the echo
-	_ = server.recv_until(b'\n',2)
-	reset_result = server.recv()
+	reset_result = remote_command('reset', server)
 	remote_term = term
-	while reset_result[-15:] == b"Terminal type? ":
+	while reset_result[-15:] == "Terminal type? ":
 		if not len(fallback):
 			exit("No fallback terminals supported by the system")
 		remote_term = fallback.pop()
 		reset_result = remote_command(remote_term, server)
-	_ = remote_command(f'export TERM="{remote_term}"', server)
+	_ = server.recv_until(str.encode(prompt))
+	_ = remote_command(f'export TERM="{remote_term}"', server, prompt=prompt)
 	return remote_term
 		
-def set_termios(local_termios, term, connection):
-	remote_term = set_term(term, connection)
+def set_termios(local_termios, term, prompt, connection):
 	local_termios[3] = local_termios[3] & ~termios.ECHO
-	termios.tcsetattr(sys.stdin, termios.TCSANOW ,local_termios)
+	termios.tcsetattr(sys.stdin, termios.TCSAFLUSH ,local_termios)
+	remote_term = set_term(term, connection, prompt)
 
 	#ioctl TIOCGWINSZ returns a struct containing the rows and cols
 	rows, cols = unpack('hh', ioctl(sys.stdin, termios.TIOCGWINSZ, '1234'))
-	_ = remote_command(f'stty rows {rows} cols {cols}', connection)
 	# TODO: hook for terminal resize
 	# TODO: what if stty isn't available
+	_ = remote_command(f'stty rows {rows} cols {cols}', connection, prompt=prompt)
 	tty.setraw(sys.stdin)
 	return remote_term
-
-def sync(connection):
-	sync_tag = uuid4().hex
-	_ = remote_command(f'\n#  {sync_tag}',connection)
-	_ = connection.recv_until(bytes(f'{sync_tag}', 'ascii'), 5)
-	_ = connection.recv()
-	# better syncing
-	print("Confirmed sync with tag:", sync_tag, end='\r\n')
-	return True
 
 def main():
 	args = handle_args()
@@ -110,15 +103,13 @@ def main():
 	nc = args.action(args)
 	print("Connected!")
 	#make sure to not record history
-	_ = remote_command(' export HISTFILE=', nc)
-	#make EXTRA sure to not record history
-	_ = remote_command('trap " export HISTFILE=" EXIT', nc)
+	_ = remote_command(' export HISTFILE=', nc, 0)
 	#detect OS
 	os = remote_command('uname -s', nc).strip()
 	# TODO: handle other OSs
 	if os not in ['Linux', 'FreeBSD', 'Darwin', 'NetBSD']:
 		exit(f'The following operating system is not supported: {os}')
-	#detect shell
+	#detect shell TODO: warn for unsupported shell
 	shell = remote_command('which $0', nc).strip()
 	# TODO: probe remote for accepted terminals
 	# TODO: check if a tty is already available
@@ -129,11 +120,11 @@ def main():
 	prompt = spawn_tty(nc, shell)
 	local_termios = termios.tcgetattr(sys.stdin)
 	try:
-		remote_term = set_termios(deepcopy(local_termios), local_term, nc)
-		if (sync(nc)):
-			# TODO: more recon
-			print(f"OS: {os} | Shell: {shell} | Term: {remote_term}", end='\r\n')
-			nc.interact()
+		remote_term = set_termios(deepcopy(local_termios), local_term, prompt, nc)
+		print(f"OS: {os} | Shell: {shell} | Term: {remote_term}", end="\r\n")
+		# FIXME: hack to get a working prompt
+		print(prompt, end='', flush=True)
+		nc.interact()
 	except Exception as e:
 		print(e)
 	finally:
